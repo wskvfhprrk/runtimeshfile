60da47b5b5d2dcdedc35f2dbdab918c3c78fb897
hejz <75412985@qq.com>
2023-02-10 22:31:49 +0800

    加上小米球运行代码

--- start.sh
+++ start.sh
@@ -13,5 +13,6 @@ cd runtime_sh_file
 mvn clean package -Dmaven.test.skip=true
 
 cd target
+ps -aux | grep runtime_sh_file | grep -v grep| awk '{print $2}' |xargs  kill -9
 #java -jar -Dserver.port=8090 runtime_sh_file-0.0.1-SNAPSHOT.jar
 nohup java -jar -Dserver.port=8090 runtime_sh_file-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
