c9df644d02f20a0702702e7e3cedc3a66e47db6a
hejz <75412985@qq.com>
2023-02-10 21:57:57 +0800

    初始化代码

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+cd /root
+rm -rf /root/runtimeshfile
+git clone git@github.com:wskvfhprrk/runtimeshfile.git
 
 
- cd /root/runtimeshfile/runtime_sh_file/src
+cd /root/runtimeshfile/runtime_sh_file/
+mvn clean package -Dmaven.test.skip=true
+
+cd target
+nohup java -jar -Dserver.port=8090 dtu-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
