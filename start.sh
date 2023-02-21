0deeffe15c4a5d7d01dc4194b546d76444efc5cc
hejz <75412985@qq.com>
2023-02-21 13:05:51 +0800

    使用tcp监测服务器

--- start.sh
+++ start.sh
@@ -11,11 +11,11 @@ cp start.sh /root/run.sh
 cd /root/
 chmod +x run.sh
 
-cd runtime_sh_file
+cd /root/runtimeshfile/runtime_sh_file
 
 mvn clean package -Dmaven.test.skip=true
 
 cd target
 ps -aux | grep runtime_sh_file | grep -v grep| awk '{print $2}' |xargs  kill -9
-#java -jar -Dserver.port=8090 runtime_sh_file-0.0.1-SNAPSHOT.jar
+
 nohup java -jar -Dserver.port=8090 runtime_sh_file-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
