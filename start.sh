151e663cfd64bbe3a1a244d96a80f99fe02d7c68
hejz <75412985@qq.com>
2023-02-22 16:57:22 +0800

    NettyClient 断线重连

--- start.sh
+++ start.sh
@@ -16,6 +16,6 @@ cd /root/runtimeshfile/runtime_sh_file
 mvn clean package -Dmaven.test.skip=true
 
 cd target
-#ps -aux | grep runtime_sh_file | grep -v grep| awk '{print $2}' |xargs  kill -9
-pkill -f runtime_sh_file
+ps -aux | grep runtime_sh_file | grep -v grep| awk '{print $2}' |xargs  kill -9
+#pkill -f runtime_sh_file
 nohup java -jar -Dserver.port=8090 runtime_sh_file-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
